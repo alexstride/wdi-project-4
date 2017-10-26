49693faee179b006b5abd3aff6c5c85d6fb47e3c
benlloydjones <benlloydjones@gmail.com>
2017-10-26 21:54:22 +0100

    fixed incorrectly named props on feedback component

--- src/components/homeworks/Feedback.js
+++ src/components/homeworks/Feedback.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Feedback = ({user, problem, feedbackSubmit, feedbackOnChange}) => {
+const Feedback = ({user, feedback, feedbackSubmit, feedbackOnChange}) => {
   return (
     <div>
-      {'pupilId' in user && problem && problem.feedback.length > 0 &&
+      {'pupilId' in user && feedback.length > 0 &&
       <div>
         <h5 className="title is-5">Your feedback:</h5>
-        <p>{problem.feedback}</p>
+        <p>{feedback}</p>
       </div>}
-      {'teacherId' in user && problem &&
+      {'teacherId' in user &&
       <form onSubmit={feedbackSubmit}>
         <div className="field">
           <input className="input">Please provide feedback:</input>
           <div className="control">
-            <textarea onChange={feedbackOnChange}className="textarea" value={problem.feedback} placeholder="please provide feedback"></textarea>
+            <textarea onChange={feedbackOnChange}className="textarea" value={feedback} placeholder="please provide feedback"></textarea>
           </div>
         </div>
       </form>}
