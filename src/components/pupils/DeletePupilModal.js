78cafec7388b0b9a4372c4764edfa52033ffaa3d
benlloydjones <benlloydjones@gmail.com>
2017-11-02 14:57:38 +0000

    centered text

--- src/components/pupils/DeletePupilModal.js
+++ src/components/pupils/DeletePupilModal.js
@@ -8,7 +8,7 @@ const DeletePupilModal = ({handleSubmit, toggleModal, modalOpen, pupil}) => {
       <div className="modal-background"></div>
       <div className="modal-content box">
         <h3
-          className="title is-3"
+          className="title is-3 pupil-delete-modal"
         >
           Are you sure you want to delete that pupil?
         </h3>
